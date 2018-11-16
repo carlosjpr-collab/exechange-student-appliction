0f3ab615b59627299932037ea123111b379b16f7
ElyesKhama <khamassi@etud.insa-toulouse.fr>
2018-11-16 17:06:25 +0100

    hello world template jdbc connection jpa set up first front page included vue.js

---
+++ src/main/java/fr/insa/soa/ExchangeSemester/hello/User.java
@@ -0,0 +1,64 @@
+package fr.insa.soa.ExchangeSemester.hello;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@Column(name = "id_user")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(name = "lastname")
+	private String lastName;
+	
+	@Column(name = "firstname")
+	private String firstName;
+
+	private String login;
+	private String password;
+	private String status;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
