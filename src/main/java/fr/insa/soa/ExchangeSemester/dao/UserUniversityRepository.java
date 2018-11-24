4f8f78553011e5f1ffa13c3ad8ba454f1b65dbf2
ElyesKhama <khamassi@etud.insa-toulouse.fr>
2018-11-24 16:58:15 +0100

    add UserUniversity entity and repository

---
+++ src/main/java/fr/insa/soa/ExchangeSemester/dao/UserUniversityRepository.java
@@ -0,0 +1,9 @@
+package fr.insa.soa.ExchangeSemester.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.insa.soa.ExchangeSemester.model.UserUniversity;
+
+public interface UserUniversityRepository extends JpaRepository<UserUniversity, Integer>{
+
+}
